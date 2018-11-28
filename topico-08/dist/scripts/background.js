095d8e7596fe4b89b7ce6e0f1228d826a7b189c3
jose eduardo <zehurzeda@gmail.com>
2018-11-28 19:40:01 -0200

    Inicio da implementação do plugin

---
+++ topico-08/dist/scripts/background.js
@@ -0,0 +1,2 @@
+"use strict";chrome.runtime.onInstalled.addListener(function(o){console.log("previousVersion",o.previousVersion)}),chrome.browserAction.setBadgeText({text:"'Allo"}),console.log("'Allo 'Allo! Event Page for Browser Action");
+//# sourceMappingURL=background.js.map
